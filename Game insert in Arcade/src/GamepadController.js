2959d16f45abebc6c9769a4e73f2fc45ec3ac805
XeuWayy <corentin.gafam@proton.me>
2024-11-01 09:26:47 +0100

    Add ✨: Game insert in Arcade base && KeyboardMouseController.js now work on every keyboard disposition.

---
+++ Game insert in Arcade/src/GamepadController.js
@@ -0,0 +1,61 @@
+class GamepadController {
+    constructor() {
+        this.current = {
+            leftStickX: 0,
+            leftStickY: 0,
+            rightStickX: 0,
+            rightStickY: 0,
+            isConnected: false
+        }
+        this.deadzone = 0.05
+        this.lookSensivity = 0.04
+
+        window.addEventListener('gamepadconnected', () => this.gamepadConnected())
+        window.addEventListener('gamepaddisconnected', () => this.gamepadDisconnected())
+    }
+
+    /**
+     * @author Corentin (XeuWayy) Charton
+     * @desc Set the gamepad state
+     */
+    gamepadConnected() {
+        this.current.isConnected = true
+    }
+
+    /**
+     * @author Corentin (XeuWayy) Charton
+     * @desc Set the gamepad state
+     */
+    gamepadDisconnected() {
+        this.current.isConnected = false
+    }
+
+    /**
+     * @author Corentin (XeuWayy) Charton
+     * @desc Check if the input is in the deadzone
+     */
+    applyDeadzone(value) {
+        return Math.abs(value) < this.deadzone ? 0 : value
+    }
+
+    /**
+     * @author Corentin (XeuWayy) Charton
+     * @desc Update the state of gamepad input
+     */
+    update() {
+        const gamepads = navigator.getGamepads()
+
+        if (!gamepads[0]) {
+            return
+        }
+        const gamepad = gamepads[0]
+
+        this.current.leftStickX = this.applyDeadzone(gamepad.axes[0])
+        this.current.leftStickY = this.applyDeadzone(gamepad.axes[1])
+
+        this.current.rightStickX = this.applyDeadzone(gamepad.axes[2]) * this.lookSensivity
+        this.current.rightStickY = this.applyDeadzone(gamepad.axes[3]) * this.lookSensivity
+    }
+}
+
+export default GamepadController
